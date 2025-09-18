c6ec602b05af69716e597ffa2ede881039b739da
oono0908 <damian_wkids@yahoo.co.jp>
2025-09-18 13:29:22 +0900

    初期環境構築完了

--- wp-content/themes/tetote/gulpfile.js
+++ wp-content/themes/tetote/gulpfile.js
@@ -4,16 +4,24 @@ import gulpSass from 'gulp-sass';
 import plumber from 'gulp-plumber';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
+import changed from 'gulp-changed';
+import imagemin, {mozjpeg, optipng} from 'gulp-imagemin';
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
 
 const sass = gulpSass(dartSass);
 
 const src = {
-  sass: ['./src//sass/**/*.scss'],
+  sass: ['./src/sass/**/*.scss'],
   sasswatch: './src/sass/**/*.scss',
+  img:'./src/images/**/*',
+  js: './src/js/**/*.js'
 };
  
 const dest = {
   css: './assets/css',
+  img: './assets/img/',
+  js: './assets/js/'
 };
 
 
@@ -34,16 +42,41 @@ const sass_task = () => {
       cascade: false
       })
     ]))
+  // .pipe(rename({
+  //           extname: '.min.js'
+  //       }))
   .pipe(gulp.dest(dest.css))
 }
 
- 
+const img_task = () => (
+    gulp.src(src.img,{ encoding: false })
+    .pipe(changed(dest.img))
+    .pipe(imagemin([
+        mozjpeg({quality: 85, progressive: true}),
+        optipng({optimizationLevel: 5})
+    ]))
+    .pipe(gulp.dest(dest.img))
+);
+
+const js_task = () => {
+  return gulp.src(src.js)
+    .pipe(uglify())
+    // .pipe(rename({
+    //         extname: '.min.js'
+    //     }))
+    .pipe(gulp.dest(dest.js));
+}
+
 const watch_task = () => {
   gulp.watch(src.sass, sass_task);
+  gulp.watch(src.img, img_task);
+  gulp.watch(src.js, js_task);
 }
  
-export default gulp.parallel(watch_task);
+export default gulp.parallel(watch_task, img_task);
 
 export {sass_task as sass};
 export {watch_task as watch};
+export {img_task as img};
+export {js_task as js};
 
